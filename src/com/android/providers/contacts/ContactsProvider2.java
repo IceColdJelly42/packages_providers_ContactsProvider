4bb5fa2f7a0c804f4295410fd03c7761d183e0fe
Steve Kondik <shade@chemlab.org>
2013-06-12 00:52:29 -0700

    contacts: Privacy Guard support
     * If privacy guard is enabled for the calling process, return an empty
       MemoryCursor (with column names populated) instead of the real dataset.
    
    Change-Id: I78b16f3791706636d51785e46a1f2ec656a3c9d2

--- src/com/android/providers/contacts/ContactsProvider2.java
+++ src/com/android/providers/contacts/ContactsProvider2.java
@@ -1962,6 +1962,10 @@ public class ContactsProvider2 extends AbstractContactsProvider
         // Enforce stream items access check if applicable.
         enforceSocialStreamWritePermission(uri);
 
+        if (getContext().isPrivacyGuardEnabled()) {
+            return null;
+        }
+
         if (mapsToProfileDbWithInsertedValues(uri, values)) {
             switchToProfileMode();
             return mProfileProvider.insert(uri, values);
@@ -1986,6 +1990,10 @@ public class ContactsProvider2 extends AbstractContactsProvider
         // Enforce stream items access check if applicable.
         enforceSocialStreamWritePermission(uri);
 
+        if (getContext().isPrivacyGuardEnabled()) {
+            return 0;
+        }
+
         if (mapsToProfileDb(uri)) {
             switchToProfileMode();
             return mProfileProvider.update(uri, values, selection, selectionArgs);
@@ -2001,6 +2009,10 @@ public class ContactsProvider2 extends AbstractContactsProvider
 
         // Enforce stream items access check if applicable.
         enforceSocialStreamWritePermission(uri);
+
+        if (getContext().isPrivacyGuardEnabled()) {
+            return 0;
+        }
 
         if (mapsToProfileDb(uri)) {
             switchToProfileMode();
@@ -4771,6 +4783,21 @@ public class ContactsProvider2 extends AbstractContactsProvider
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
             String sortOrder, CancellationSignal cancellationSignal) {
+        Cursor c = queryInternal(uri, projection, selection, selectionArgs,
+                sortOrder, cancellationSignal);
+
+        if (getContext().isPrivacyGuardEnabled()) {
+            Log.d(TAG, "Contacts query from application with privacy guard! pid=" + Binder.getCallingPid());
+            MemoryCursor mc = new MemoryCursor(null, c.getColumnNames());
+            c.close();
+            return mc;
+        }
+
+        return c;
+    }
+
+    private Cursor queryInternal(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                String sortOrder, CancellationSignal cancellationSignal) {
         if (VERBOSE_LOGGING) {
             Log.v(TAG, "query: uri=" + uri + "  projection=" + Arrays.toString(projection) +
                     "  selection=[" + selection + "]  args=" + Arrays.toString(selectionArgs) +
